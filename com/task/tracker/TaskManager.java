7e0ff7770deafdb82bfadb167a9ae513f98e95f1
AbhimanyuPatidar <abhimanyupatidar2511@gmail.com>
2024-10-12 13:33:45 +0530

    Shift File IO related methods to TaskManagers

---
+++ com/task/tracker/TaskManager.java
@@ -0,0 +1,28 @@
+package com.task.tracker;
+
+public class TaskManager {
+    public void addTask(String description, String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addTask'");
+    }
+
+    public void updateTask(int id, String desciption, Object status) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateTask'");
+    }
+
+    public void deleteTask(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteTask'");
+    }
+
+    public void listTasks() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listTasks'");
+    }
+
+    public void listTasksByStatus(String status) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listTasksByStatus'");
+    }
+}
--- com/task/tracker/TaskService.java
+++ com/task/tracker/TaskService.java
@@ -21,6 +21,7 @@ public class TaskService {
 
     // Will check for args and call the respective method
     public void execute(String[] args) throws InvNumOfArgsException, InvUseOfOptionException, InvCmdPassedException {
+        TaskManager taskManager = new TaskManager();
         
         if (!args[0].equalsIgnoreCase("add") && !args[0].equalsIgnoreCase("update") && !args[0].equalsIgnoreCase("delete") && !args[0].equalsIgnoreCase("list")) {
             throw new InvCmdPassedException("Invalid command provided. Please provide a valid command.");
@@ -32,10 +33,10 @@ public class TaskService {
 
             if (args.length == 2) {
                 // Default status 'pending' applied
-                addTask(description, "pending");
+                taskManager.addTask(description, "pending");
             } else if (args.length == 3) {
                 String status = args[2];
-                addTask(description, status);
+                taskManager.addTask(description, status);
             } else {
                 throw new InvNumOfArgsException("Invalid number of arguments provided for 'add' command.");
             }
@@ -51,11 +52,11 @@ public class TaskService {
                 if (args[2].charAt(1) == 'd' || args[2].charAt(1) == 'D') {
                     // Updating description
                     String newDescription = args[3];
-                    updateTask(id, newDescription, null);
+                    taskManager.updateTask(id, newDescription, null);
                 } else if (args[2].charAt(1) == 's' || args[2].charAt(1) == 'S') {
                     // Updating status
                     String newStatus = args[3];
-                    updateTask(id, null, newStatus);
+                    taskManager.updateTask(id, null, newStatus);
                 } else {
                     // Invalid use of '-'
                     throw new InvUseOfOptionException("Invalid use of '-' in 'update' command.");
@@ -64,7 +65,7 @@ public class TaskService {
                 // Updating both description and status
                 String newDescription = args[2];
                 String newStatus = args[3];
-                updateTask(id, newDescription, newStatus);
+                taskManager.updateTask(id, newDescription, newStatus);
             }
         } else {
             throw new InvNumOfArgsException("Insufficient number of arguments provided for 'update' command.");
@@ -73,44 +74,19 @@ public class TaskService {
         // For 'delete' command
         if (args[0].equalsIgnoreCase("delete") && args.length == 2) {
             Integer id = Integer.parseInt(args[1]);
-            deleteTask(id);
+            taskManager.deleteTask(id);
         } else {
             throw new InvNumOfArgsException("Invalid number of arguments provided for 'delete' command.");
         }
 
         // To list all tasks or by status
         if (args[0].equalsIgnoreCase("list") && args.length == 1) {
-            listTasks();
+            taskManager.listTasks();
         } else if (args[0].equalsIgnoreCase("list") && args.length == 2) {
             String status = args[1];
-            listTasksByStatus(status);
+            taskManager.listTasksByStatus(status);
         } else {
             throw new InvNumOfArgsException("Invalid number of arguments provided for 'list' command.");
         }
-    }
-
-    private int addTask(String description, String string) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addTask'");
-    }
-
-    private void updateTask(int id, String desciption, Object status) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateTask'");
-    }
-
-    private void deleteTask(int id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteTask'");
-    }
-
-    private void listTasks() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listTasks'");
-    }
-
-    private void listTasksByStatus(String status) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'listTasksByStatus'");
     }
 }
